9b3f9edc2627f14bb3aaaa8f95ea889eb3190df6
duckboycool <duckboycool101@gmail.com>
2026-01-11 10:02:12 -0700

    Ignore tags inside markdown code blocks

--- messageListeners/messageTags.js
+++ messageListeners/messageTags.js
@@ -1,17 +1,16 @@
 const { dbQueryOne } = require('../lib');
 
 module.exports = async (client, message) => {
-  const matches = message.content.match(/&\w+\s?/g);
+  // Preprocess string to remove code blocks
+  // This detection is definitely not perfect, but hopefully it'll be enough to suffice in practice
+  // If not, the best option would probably be to bring in a discord markdown parsing lib
+  const searchContent = message.content.replaceAll(/(?<!\\)(`{1,3}).+?\1/gs, '');
+  // Extract and deduplicate tags present
+  const messageTags = new Set(searchContent.match(/(?<=\B&)\w+/g));
 
   // If there are no tags in this message, do nothing
-  if (!matches) { return; }
+  if (messageTags.size === 0) { return; }
 
-  // Remove the & and trim tags, remove duplicates with a Set
-  const messageTags = new Set();
-  matches.forEach((tag) => {
-    messageTags.add(tag.substring(1).trim());
-  });
-  
   // Send each tag message to the channel in a separate message, in order of message
   for (let tag of messageTags) {
     // Fetch the tag message from the database
