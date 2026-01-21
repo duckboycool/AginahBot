e9bd46273121e18987bb155102d7a76fa9a74d23
duckboycool <duckboycool101@gmail.com>
2026-01-20 21:15:34 -0700

    Add command version of thread purge

--- slashCommandCategories/utility.js
+++ slashCommandCategories/utility.js
@@ -1,7 +1,7 @@
-const { SlashCommandBuilder, MessageFlags, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, InteractionContextType, MessageFlags, PermissionFlagsBits } = require('discord.js');
 const tmp = require('tmp');
 const fs = require('fs');
-const { dbExecute } = require('../lib');
+const { dbExecute, replyError } = require('../lib');
 
 module.exports = {
   category: 'Utility Commands',
@@ -97,6 +97,82 @@ module.exports = {
     },
     {
       commandBuilder: new SlashCommandBuilder()
+        .setName('purge-thread')
+        .setDescription('Remove all members (except mods) from the current thread who are inactive.')
+        .addIntegerOption((opt) => opt
+          .setName('days')
+          .setDescription('Number of days of inactivity for cutoff. Min 1, default 60')
+          .setRequired(false))
+        .addBooleanOption((opt) => opt
+          .setName('dry-run')
+          .setDescription('Run a test to see how many users would be removed without actually removing them')
+          .setRequired(false))
+        .setContexts(InteractionContextType.Guild)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+      async execute(interaction) {
+        const days = interaction.options.getInteger('days') ?? 60;
+        const doPurge = !interaction.options.getBoolean('dry-run');
+
+        if (days < 1) {
+          return interaction.reply({
+            content: 'Invalid activity cutoff.',
+            flags: MessageFlags.Ephemeral,
+          });
+        }
+
+        if (!interaction.channel.isThread()) {
+          return interaction.reply({
+            content: 'Cannot purge non-thread channel.',
+            flags: MessageFlags.Ephemeral,
+          });
+        }
+
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+        console.info('Running purge...');
+
+        let totalPurgedMembers = 0;
+
+        try {
+          const targetDate = Math.floor(new Date().getTime() - (days * 24 * 60 * 60 * 1000));
+          console.info(`\nFetching messages since ${new Date(targetDate).toISOString()}`);
+          const messages = await fetchMessagesSince(interaction.channel, targetDate);
+          console.info(`Found ${messages.length} messages total`);
+
+          // Fetch full user list for thread
+          console.info('\nFetching thread members');
+          const threadMembers = await fetchThreadMembers(interaction.channel);
+          console.debug(`Found ${threadMembers.length} members`);
+
+          // Identify unique active thread members
+          const activeUsers = new Set();
+          messages.forEach((msg) => {
+            activeUsers.add(msg.author.id);
+          });
+          console.debug(`\n${activeUsers.size} unique users have sent messages in given period`);
+
+          // Remove thread members who have not sent a message in specified time
+          console.info('Purging inactive members...');
+          for (let member of threadMembers) {
+            if (!activeUsers.has(member.id)) {
+              console.info(`Removing ${member.user.username}`);
+              totalPurgedMembers++;
+              if (doPurge) {
+                await interaction.channel.members.remove(member.id);
+                await new Promise((resolve) => setTimeout(resolve, 600));
+              }
+            }
+          }
+        } catch (e) {
+          console.error(e);
+          return interaction.followUp(replyError('Something went wrong and the thread could not be purged.\n'));
+        }
+
+        console.info(`Purged ${totalPurgedMembers} users`);
+        return interaction.followUp(`Purged ${totalPurgedMembers} users`);
+      }
+    },
+    {
+      commandBuilder: new SlashCommandBuilder()
         .setName('set-moderator-role')
         .setDescription('Assign a new role to use as the Moderator role when determining moderator actions.')
         .addRoleOption((opt) => opt
@@ -115,4 +191,64 @@ module.exports = {
       }
     },
   ],
+};
+
+// Subroutines copied over from purge script
+const fetchMessagesSince = async (threadChannel, oldestTimestamp, limit=100, messageCache=[]) => {
+  // Fetch messages prior to the oldest (first) message in the cache
+  const messages = await threadChannel.messages.fetch({
+    limit: limit,
+    before: messageCache[0]?.id || null,
+  });
+
+  // Prepend newly fetched messages to the front of a working array
+  const msgArray = [];
+  const foundMessages = messages.map((m) => m).reverse();
+  for (let msg of foundMessages) {
+    if (msg.createdTimestamp >= oldestTimestamp) {
+      msgArray.push(msg);
+    }
+  }
+  msgArray.push(...messageCache);
+
+  // If no more messages are available, return what was found
+  if (foundMessages.length < limit) {
+    // Return messages
+    return msgArray;
+  }
+
+  // Wait half a second to prevent rate-limiting
+  await new Promise((resolve) => setTimeout(resolve, 600));
+
+  // Fetch more messages if the desired timestamp has not been reached
+  if (foundMessages[0].createdTimestamp >= oldestTimestamp) {
+    console.info(`\n${new Date(msgArray[0].createdTimestamp).toISOString()}`);
+    console.info(`Message count: ${msgArray.length}`);
+    return await fetchMessagesSince(threadChannel, oldestTimestamp, limit, msgArray);
+  }
+
+  // Return messages
+  return msgArray;
+};
+
+const fetchThreadMembers = async (threadChannel, limit=100, userCache=[]) => {
+  const members = await threadChannel.members.fetch({
+    limit,
+    after: userCache[userCache.length-1]?.id || null,
+    withMember: true,
+  });
+
+  const userArray = [...userCache];
+  members.each((member) => {
+    if (!member.bot) {
+      userArray.push(member);
+    }
+  });
+
+  if (members.size < limit) {
+    return userArray;
+  }
+
+  console.info(`User count: ${userArray.length}`);
+  return await fetchThreadMembers(threadChannel, limit, userArray);
 };
